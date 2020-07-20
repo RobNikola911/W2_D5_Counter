40103319ad95fc23d919cbdc182c97315c188e1b
RobNikola911 <pajic1991@gmail.com>
2020-07-20 20:19:52 +0200

    Finished

---
+++ script.js
@@ -0,0 +1,25 @@
+/* document.getElementById("myBtn").addEventListener("click", displayDate);
+
+function displayDate() {
+  document.getElementById("demo").innerHTML = Date(); */
+
+/*   $(document).ready(function(){
+    $("h1").on("click", function(){
+      $(this).hide();
+    });
+  }); */
+/* 
+  $( "#myBtn" ).on('click', (function() {
+    $('#demo').text('New Text');
+})); */
+
+/* let demo = 0;
+
+$(document).ready(function () {
+    $("#myBtn").click(function() {
+    let increase = $('h1').html;
+    demo++;
+     document.title = '(' +sayi+ ')' + baslik;
+    });
+}); */
+
